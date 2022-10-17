befb360d27638a0eaf08a0d6e308e0e8ad214572
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-10-17 14:21:50 -0300

    Exemplo De Data-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * String Interpolation: utiliazar valores que 
+   */
   title = 'data-binding';
+  n1:number = 0
+  n2:number = 0
+  tipoInput:String  = "password"
+
+
+  somar(x:number, y:number){
+    return x+y
+  }
+          //esconder senha com Angula 
+  mudarTipoDoInput(): void{
+    if(this.tipoInput == 'password')
+    {this.tipoInput = 'text'
+  }else{
+    this.tipoInput= 'password'
+  }
+  }  
 }
